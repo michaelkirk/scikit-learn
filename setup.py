37d284f80ac6183781392f5821a00ee0aa1e561e
michaelkirk <michael.john.kirk@gmail.com>
2012-11-19 00:32:06 +0800

    install requires numpy

--- setup.py
+++ setup.py
@@ -146,4 +146,5 @@ if __name__ == "__main__":
               'Operating System :: Unix',
               'Operating System :: MacOS'
              ],
+          install_requires=[ 'numpy' ]
       **extra_setuptools_args)
